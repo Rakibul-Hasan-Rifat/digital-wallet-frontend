a8c44ef3e4fb82a10aded4a472418dc101fbf166
Rakibul Hasan <rakibulhasan745101@gmail.com>
2025-08-24 10:21:16 +0600

    redux initialized

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import router from './routes/index.ts'
 import { ThemeProvider } from './providers/theme.provider.tsx'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from './redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <ReduxProvider store={store}>
+
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </ReduxProvider>
   </StrictMode>,
 )
